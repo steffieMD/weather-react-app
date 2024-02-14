ceba5d3a8d75109a3bc6e951b9402b8c16b8677b
Stephanie Metieh <stephaniemetieh@gmail.com>
2024-02-14 14:03:46 +0100

    created react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  let [temp, setTemp] = useState(null);
+
+  const showTemperature = (response) => {
+    setTemp(response.data.main.temp);
+  };
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>The weather is Lagos is {temp}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
